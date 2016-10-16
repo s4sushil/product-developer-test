6a81677a22e8f9603e671192b0a78a5fda0a87ee
Sushil Choudhary <s4sushil@yahoo.com>
2016-10-16 19:53:52 +0100

    Initial logic to find the location

--- src/main/java/com/whitbread/foursquare/service/LocationFinderServiceImpl.java
+++ src/main/java/com/whitbread/foursquare/service/LocationFinderServiceImpl.java
@@ -18,6 +18,79 @@ import com.whitbread.foursquare.model.Location;
 public class LocationFinderServiceImpl implements LocationFinderService
 {
 
-  
+    /**
+     * Client Id for foursquare
+     */
+    private String CLIENT_ID = "2YOLMASACQYB1NQK2H22KS2VEYVF50WT2FMXPNA3B31UBVXF";
+
+    /**
+     * Client Secret for foursquare
+     */
+    private String CLIENT_SECRET = "CQPPNDNS4XHEGTXPUM0IPTRNDQHGK3UGBFJFEBTP4AN2SNLN";
+
+    public List<Location> findPlacesByName(final String name) throws Exception
+    {
+	if (StringUtils.isEmpty(name))
+	{
+	    throw new IllegalArgumentException("Illegal Argument param");
+	}
+
+	final Client client = Client.create();
+	final WebResource webResource = client.resource(
+		"https://api.foursquare.com/v2/venues/search?client_id="
+			+ CLIENT_ID + "&client_secret=" + CLIENT_SECRET
+			+ "&v=20161016&near=" + name);
+
+	// tested with OAuth first.
+	final WebResource webResource22 = client
+		.resource("https://api.foursquare.com/v2/venues/search?"
+			+ "oauth_token=H33IHO0C0PWN5QIP0W1HTRBZZYOD3D24DEH4ERWHBNXLKIJC&v=20161016&near="
+			+ name);
+
+	ClientResponse response = webResource.accept("application/json")
+		.get(ClientResponse.class);
+
+	if (response.getStatus() != 200)
+	{
+	    throw new RuntimeException(
+		    "Failed : HTTP error code : " + response.getStatus());
+	}
+
+	String output = response.getEntity(String.class);
+	return convertToJava(output);
+    }
+
+    /**
+     * Converts the json object into location pojo.
+     * 
+     * @param json as String
+     * @return list of location
+     */
+    private List<Location> convertToJava(final String jsonString)
+	    throws Exception
+    {
+	final List<Location> places = new ArrayList<Location>();
+
+	// Create json related objects
+	ObjectMapper objectMapper = new ObjectMapper();
+	JsonNode rootNode = objectMapper.readTree(jsonString);
+	JsonNode venuesNode = rootNode.findPath("venues");
+	Iterator<JsonNode> venues = venuesNode.getElements();
+
+	while (venues.hasNext())
+	{
+	    JsonNode venue = venues.next();
+	    JsonNode name = venue.findPath("name");
+	    JsonNode location = venue.findPath("location");
+
+	    Location place = new Location();
+	    place.setLatitude(location.findPath("lat").asText());
+	    place.setLongitude(location.findPath("lng").asText());
+	    place.setName(name.asText());
+	    places.add(place);
+	}
+
+	return places;
+    }
 
 }
--- src/main/webapp/locationFinder.jsp
+++ src/main/webapp/locationFinder.jsp
@@ -12,6 +12,65 @@
 
 </head>
 <body>
+
+<!-- Location finder Area -->
+<div class="row">
+  <div class="col-lg-12" style="text-align:center;">
+  <h2>Location Finder App !! </h2>
+  </div>
+    
+</div>
+<div class="row">
+   	<form action="search" method="post">
+	<div class="col-lg-12">
+    	<div class="input-group">
+	      		<input type="text" class="form-control" value="${location}" name="location" placeholder="Search for...">
+	      		<span class="input-group-btn">
+	        		<input class="btn btn-default" type="submit">Search!</input>
+	      		</span>
+    	</div>
+  	</div>
+    </form>	
+</div>
+
+
+<!-- Map Area Code taken from bootstrap example code -->
+<div id="map-canvas" style="height:600px; width:100%"></div>
+
+<script>
+	var locations = [	
+		<c:forEach var="location" items="${searchedResults}" varStatus="loop">
+	   		["${location.name}", ${location.latitude}, ${location.longitude}, ${loop.index}],
+		</c:forEach>
+	];
+	
+	if (locations.length==0) {
+		document.getElementById('map-canvas').innerHTML = "<div style=\"text-align:center;\"><h3>No results found.</h3>";	
+	}
+               var map = new google.maps.Map(document.getElementById('map-canvas'), {
+                 zoom: 12,
+                 center: new google.maps.LatLng(locations[0][1], locations[0][2]),
+                 mapTypeId: google.maps.MapTypeId.ROADMAP
+               });
+
+               var infowindow = new google.maps.InfoWindow();
+
+               var marker, i;
+
+               for (i = 0; i < locations.length; i++) {  
+                 marker = new google.maps.Marker({
+                   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                   map: map
+                 });
+
+                 google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                   return function() {
+                     infowindow.setContent(locations[i][0]);
+                     infowindow.open(map, marker);
+                   }
+                 })(marker, i));
+               }
+	</script>
 </body>
 
 </html>
